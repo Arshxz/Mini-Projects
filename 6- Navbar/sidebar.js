c6797fcf53faba46f3b2c7f32bf1599d98e561b3
Arshxz <55192108+Arshxz@users.noreply.github.com>
2021-06-26 13:19:55 +0530

    Add files via upload

---
+++ 6- Navbar/sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { links, social } from './data'
+
+function sidebar(props) {
+    return (
+        <>
+            <h4>Sidebar</h4>
+            <div className="links-container show-container">
+                <ul className="links">
+                {links.map((link)=>{
+                    const {id,url,text} = link;
+                    return(
+                    <li key={id}>
+                        <a href={url}>{text}</a>
+                    </li>
+                    )
+                })}
+                </ul>
+            </div>
+            <ul className="social-icons">
+          {social.map((socialIcon)=>{
+            const {id,url,icon} = socialIcon
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+        </>
+
+    );
+}
+
+export default sidebar;
